145302ed64df65949711490c2d067a591be1fd31
PinewoodPip <53646914+PinewoodPip@users.noreply.github.com>
2020-12-02 09:43:48 +0100

    Add weapon boost parsing to rune script

--- public/Scripts/Runes/runes.py
+++ public/Scripts/Runes/runes.py
@@ -6,6 +6,471 @@ weapons = open("Weapon.txt")
 armors = open("Armor.txt")
 consumes = open("Status_CONSUME.txt")
 
+REMOVE_SCAFFOLDING_KEYS = True
+
+# lifesteal on bone runes is repeated
+
+# todo check viality boost
+# check why 1 bone rune does not have the boost in output
+# bone rune gives same vit?
+
+# excluded are masterwork, maxvit
+
+# dict to translate weapon boosts to flexStat IDs
+REAL_STAT_IDS = {
+    "StrengthBoost": "STRENGTH",
+    "MemoryBoost": "MEMORY",
+    "FinesseBoost": "FINESSE",
+    "ConstitutionBoost": "CONSTITUTION",
+    "IntelligenceBoost": "INTELLIGENCE",
+    "WitsBoost": "WITS",
+    "Fire": "FIRERESISTANCE",
+    "Water": "WATERRESISTANCE",
+    "Air": "AIRRESISTANCE",
+    "Earth": "EARTHRESISTANCE",
+    "Poison": "POISONRESISTANCE",
+    "Initiative": "INITIATIVE",
+    "Leadership": "Leadership",
+    # "ArmorBoost": "PHYSICALARMORBOOST",
+    # "MagicArmorBoost": "MAGICARMORBOOST",
+    "DualWielding": "DualWielding",
+    "TwoHanded": "TwoHanded",
+    "Perseverance": "Perseverance",
+    # "LifeSteal": "LIFESTEAL", # already handled by AMER_DELDUM. unsure why it appears as both script boost and game boost
+    "Ranged": "Ranged",
+    "SingleHanded": "SingleHanded",
+
+    # "VitalityBoost": "VITALITYBOOST",
+    "Piercing": "PIERCINGRESISTANCE",
+    "PainReflection": "PainReflection",
+    "DodgeBoost": "DODGEBOOST",
+
+    "CriticalChance": "CRITICALCHANCE",
+    "Movement": "MOVEMENT",
+}
+
+# special modifiers handled by script. this dict assigns them the data format we use in the app
+DELDUMS = {
+    "AMER_DELDUM_RUNEDUMMY_FLAME_SMALL": [
+        {
+            "type": "extendedStat",
+            "id": "FreeReactionCharge_AMER_Elementalist",
+            "value": 1
+        },
+    ],
+    "AMER_DELDUM_RUNEDUMMY_FLAME_MEDIUM": [
+        {
+            "type": "extendedStat",
+            "id": "FreeReactionCharge_AMER_Elementalist",
+            "value": 1
+        },
+        {
+            "type": "specialLogic",
+            "id": "PIP_Rune_EleCrit",
+            "keyword": "Elementalist",
+            "keywordBoon": "mutator",
+            "value": 15,
+        }
+    ],
+    "AMER_DELDUM_RUNEDUMMY_FLAME_LARGE": [
+        {
+            "type": "extendedStat",
+            "id": "FreeReactionCharge_AMER_Elementalist",
+            "value": 1
+        },
+        {
+            "type": "specialLogic",
+            "id": "PIP_Rune_EleCrit",
+            "keyword": "Elementalist",
+            "keywordBoon": "mutator",
+            "value": 30,
+        }
+    ],
+    "AMER_DELDUM_RUNEDUMMY_FLAME_GIANT": [
+        {
+            "type": "extendedStat",
+            "id": "FreeReactionCharge_AMER_Elementalist",
+            "value": 1
+        },
+        {
+            "type": "specialLogic",
+            "id": "PIP_Rune_EleCrit",
+            "keyword": "Elementalist",
+            "keywordBoon": "mutator",
+            "value": 45,
+        }
+    ],
+    "AMER_DELDUM_RUNEDUMMY_FROST_SMALL": [
+        {
+            "type": "extendedStat",
+            "id": "FreeReactionCharge_AMER_Celestial",
+            "value": 1
+        },
+    ],
+    "AMER_DELDUM_RUNEDUMMY_FROST_MEDIUM": [
+        {
+            "type": "extendedStat",
+            "id": "FreeReactionCharge_AMER_Celestial",
+            "value": 1
+        },
+        {
+            "type": "specialLogic",
+            "id": "PIP_Rune_CelestialVitality",
+            "keyword": "Celestial",
+            "keywordBoon": "mutator",
+            "value": 7,
+        }
+    ],
+    "AMER_DELDUM_RUNEDUMMY_FROST_LARGE": [
+        {
+            "type": "extendedStat",
+            "id": "FreeReactionCharge_AMER_Celestial",
+            "value": 1
+        },
+        {
+            "type": "specialLogic",
+            "id": "PIP_Rune_CelestialVitality",
+            "keyword": "Celestial",
+            "keywordBoon": "mutator",
+            "value": 14,
+        }
+    ],
+    "AMER_DELDUM_RUNEDUMMY_FROST_GIANT": [
+        {
+            "type": "extendedStat",
+            "id": "FreeReactionCharge_AMER_Celestial",
+            "value": 1
+        },
+        {
+            "type": "specialLogic",
+            "id": "PIP_Rune_CelestialVitality",
+            "keyword": "Celestial",
+            "keywordBoon": "mutator",
+            "value": 21,
+        }
+    ],
+    "AMER_DELDUM_RUNEDUMMY_EARTH_SMALL": [
+        {
+            "type": "extendedStat",
+            "id": "FreeReactionCharge_AMER_Centurion",
+            "value": 1
+        },
+    ],
+    "AMER_DELDUM_RUNEDUMMY_EARTH_MEDIUM": [
+        {
+            "type": "extendedStat",
+            "id": "FreeReactionCharge_AMER_Centurion",
+            "value": 1
+        },
+        {
+            "type": "specialLogic",
+            "id": "PIP_Rune_CenturionRestoreArmor",
+            "keyword": "Centurion",
+            "keywordBoon": "mutator",
+            "value": 5,
+        }
+    ],
+    "AMER_DELDUM_RUNEDUMMY_EARTH_LARGE": [
+        {
+            "type": "extendedStat",
+            "id": "FreeReactionCharge_AMER_Centurion",
+            "value": 1
+        },
+        {
+            "type": "specialLogic",
+            "id": "PIP_Rune_CenturionRestoreArmor",
+            "keyword": "Centurion",
+            "keywordBoon": "mutator",
+            "value": 10,
+        }
+    ],
+    "AMER_DELDUM_RUNEDUMMY_EARTH_GIANT": [
+        {
+            "type": "extendedStat",
+            "id": "FreeReactionCharge_AMER_Centurion",
+            "value": 1
+        },
+        {
+            "type": "specialLogic",
+            "id": "PIP_Rune_CenturionRestoreArmor",
+            "keyword": "Centurion",
+            "keywordBoon": "mutator",
+            "value": 15,
+        }
+    ],
+    "AMER_DELDUM_RUNEDUMMY_AIR_SMALL": [
+        {
+            "type": "extendedStat",
+            "id": "FreeReactionCharge_AMER_Predator",
+            "value": 1
+        },
+    ],
+    "AMER_DELDUM_RUNEDUMMY_AIR_MEDIUM": [
+        {
+            "type": "extendedStat",
+            "id": "FreeReactionCharge_AMER_Predator",
+            "value": 1
+        },
+        {
+            "type": "specialLogic",
+            "id": "PIP_Rune_PredatorPowerDamage",
+            "keyword": "Predator",
+            "keywordBoon": "mutator",
+            "value": 15,
+        }
+    ],
+    "AMER_DELDUM_RUNEDUMMY_AIR_LARGE": [
+        {
+            "type": "extendedStat",
+            "id": "FreeReactionCharge_AMER_Predator",
+            "value": 1
+        },
+        {
+            "type": "specialLogic",
+            "id": "PIP_Rune_PredatorPowerDamage",
+            "keyword": "Predator",
+            "keywordBoon": "mutator",
+            "value": 30,
+        }
+    ],
+    "AMER_DELDUM_RUNEDUMMY_AIR_GIANT": [
+        {
+            "type": "extendedStat",
+            "id": "FreeReactionCharge_AMER_Predator",
+            "value": 1
+        },
+        {
+            "type": "specialLogic",
+            "id": "PIP_Rune_PredatorPowerDamage",
+            "keyword": "Predator",
+            "keywordBoon": "mutator",
+            "value": 45,
+        }
+    ],
+    "AMER_DELDUM_RUNEDUMMY_VENOM_SMALL": [
+        {
+            "type": "extendedStat",
+            "id": "FreeReactionCharge_AMER_Occultist",
+            "value": 1
+        },
+    ],
+    "AMER_DELDUM_RUNEDUMMY_VENOM_MEDIUM": [
+        {
+            "type": "extendedStat",
+            "id": "FreeReactionCharge_AMER_Occultist",
+            "value": 1
+        },
+        {
+            "type": "specialLogic",
+            "id": "PIP_Rune_OccultistInstantDamage",
+            "keyword": "Occultist",
+            "keywordBoon": "mutator",
+            "value": 50,
+        }
+    ],
+    "AMER_DELDUM_RUNEDUMMY_VENOM_LARGE": [
+        {
+            "type": "extendedStat",
+            "id": "FreeReactionCharge_AMER_Occultist",
+            "value": 1
+        },
+        {
+            "type": "specialLogic",
+            "id": "PIP_Rune_OccultistInstantDamage",
+            "keyword": "Occultist",
+            "keywordBoon": "mutator",
+            "value": 75,
+        }
+    ],
+    "AMER_DELDUM_RUNEDUMMY_VENOM_GIANT": [
+        {
+            "type": "extendedStat",
+            "id": "FreeReactionCharge_AMER_Occultist",
+            "value": 1
+        },
+        {
+            "type": "specialLogic",
+            "id": "PIP_Rune_OccultistInstantDamage",
+            "keyword": "Occultist",
+            "keywordBoon": "mutator",
+            "value": 100,
+        }
+    ],
+    # lifesteal for bone runes
+    "AMER_DELDUM_LIFESTEAL_5": [
+        {
+            "type": "flexStat",
+            "id": "LIFESTEAL",
+            "value": 5
+        },
+    ],
+    "AMER_DELDUM_LIFESTEAL_7": [
+        {
+            "type": "flexStat",
+            "id": "LIFESTEAL",
+            "value": 7
+        },
+    ],
+    "AMER_DELDUM_LIFESTEAL_10": [
+        {
+            "type": "flexStat",
+            "id": "LIFESTEAL",
+            "value": 10
+        },
+    ],
+    "AMER_DELDUM_LIFESTEAL_14": [
+        {
+            "type": "flexStat",
+            "id": "LIFESTEAL",
+            "value": 14
+        },
+    ],
+    # regen
+    "AMER_DELDUM_REGEN_LIFE_3": [
+        {
+            "type": "extendedStat",
+            "id": "Regen_Life",
+            "value": 3,
+        }
+    ],
+    "AMER_DELDUM_REGEN_LIFE_4": [
+        {
+            "type": "extendedStat",
+            "id": "Regen_Life",
+            "value": 4,
+        }
+    ],
+    "AMER_DELDUM_REGEN_LIFE_5": [
+        {
+            "type": "extendedStat",
+            "id": "Regen_Life",
+            "value": 5,
+        }
+    ],
+    "AMER_DELDUM_REGEN_LIFE_6": [
+        {
+            "type": "extendedStat",
+            "id": "Regen_Life",
+            "value": 6,
+        }
+    ],
+    # magic armor regen
+    "AMER_DELDUM_REGEN_MAGIC_4.5": [
+        {
+            "type": "extendedStat",
+            "id": "Regen_MagicArmor",
+            "value": 4.5,
+        },
+    ],
+    "AMER_DELDUM_REGEN_MAGIC_6": [
+        {
+            "type": "extendedStat",
+            "id": "Regen_MagicArmor",
+            "value": 6,
+        },
+    ],
+    "AMER_DELDUM_REGEN_MAGIC_7.5": [
+        {
+            "type": "extendedStat",
+            "id": "Regen_MagicArmor",
+            "value": 7.5,
+        },
+    ],
+    "AMER_DELDUM_REGEN_MAGIC_9": [
+        {
+            "type": "extendedStat",
+            "id": "Regen_MagicArmor",
+            "value": 9,
+        },
+    ],
+    # physical armor regen
+    "AMER_DELDUM_REGEN_PHYS_4.5": [
+        {
+            "type": "extendedStat",
+            "id": "Regen_PhysicalArmor",
+            "value": 4.5,
+        },
+    ],
+    "AMER_DELDUM_REGEN_PHYS_6": [
+        {
+            "type": "extendedStat",
+            "id": "Regen_PhysicalArmor",
+            "value": 6,
+        },
+    ],
+    "AMER_DELDUM_REGEN_PHYS_7.5": [
+        {
+            "type": "extendedStat",
+            "id": "Regen_PhysicalArmor",
+            "value": 7.5,
+        },
+    ],
+    "AMER_DELDUM_REGEN_PHYS_9": [
+        {
+            "type": "extendedStat",
+            "id": "Regen_PhysicalArmor",
+            "value": 9,
+        },
+    ],
+    # resistance reduction
+    "AMER_DELDUM_RUNERESIST_PHYSICAL_SMALL": [
+        {
+            "type": "extendedStat",
+            "id": "PIP_PhysResReductionOnHit",
+            "value": 6,
+        },
+    ],
+    "AMER_DELDUM_RUNERESIST_PHYSICAL_MEDIUM": [
+        {
+            "type": "extendedStat",
+            "id": "PIP_PhysResReductionOnHit",
+            "value": 8,
+        },
+    ],
+    "AMER_DELDUM_RUNERESIST_PHYSICAL_LARGE": [
+        {
+            "type": "extendedStat",
+            "id": "PIP_PhysResReductionOnHit",
+            "value": 10,
+        },
+    ],
+    "AMER_DELDUM_RUNERESIST_PHYSICAL_GIANT": [
+        {
+            "type": "extendedStat",
+            "id": "PIP_PhysResReductionOnHit",
+            "value": 12,
+        },
+    ],
+    "AMER_DELDUM_RUNERESIST_ELE_SMALL": [
+        {
+            "type": "extendedStat",
+            "id": "PIP_EleResReductionOnHit",
+            "value": 6,
+        },
+    ],
+    "AMER_DELDUM_RUNERESIST_ELE_MEDIUM": [
+        {
+            "type": "extendedStat",
+            "id": "PIP_EleResReductionOnHit",
+            "value": 8,
+        },
+    ],
+    "AMER_DELDUM_RUNERESIST_ELE_LARGE": [
+        {
+            "type": "extendedStat",
+            "id": "PIP_EleResReductionOnHit",
+            "value": 10,
+        },
+    ],
+    "AMER_DELDUM_RUNERESIST_ELE_GIANT": [
+        {
+            "type": "extendedStat",
+            "id": "PIP_EleResReductionOnHit",
+            "value": 12,
+        },
+    ],
+}
+
 SIZES = ["Small", "Medium", "Large", "Giant"]
 RUNES = [
     "Flame", "Frost", "Thunder", "Rock", "Venom", "Masterwork", "Bloodstone", "Ruby", "Lapis", "Pearl", "Sapphire", "Silver", "Clay", "Granite", "Iron", "Bone", "Obsidian", "Onyx", "Steel", "Gold", "TigersEye", "Topaz", "Emerald", "Jade", "Malachite",
@@ -13,7 +478,8 @@ RUNES = [
 
 IGNORED_PROPERTIES = [
     "Value",
-    "ExtraProperties"
+    "ExtraProperties",
+    "Physical",
 ]
 
 runeRegex = re.compile(
@@ -92,7 +558,7 @@ for txt in [0, 1]:
                 else:
                     rune[key] = {search["id"]: search["value"]}
 
-# todo figure out where special strings like +free predator come from
+# list of all special properties like armor regen
 specialEffects = []
 for rune in runes:
     if "real_weaponEffect" in runes[rune]:
@@ -105,10 +571,10 @@ currentStatus = None
 displayRegex = re.compile('data "DisplayNameRef" "(?P<value>.*)"')
 for line in consumes.readlines():
     if weaponEntryRegex.search(line):
-        if weaponEntryRegex.search(line).groupdict()["id"] in specialEffects:
-            currentStatus = weaponEntryRegex.search(line).groupdict()["id"]
-        else:
-            currentStatus = None
+        # if weaponEntryRegex.search(line).groupdict()["id"] in specialEffects:
+        currentStatus = weaponEntryRegex.search(line).groupdict()["id"]
+        # else:
+            # currentStatus = None
     elif displayRegex.search(line) and currentStatus:
         statuses[currentStatus] = displayRegex.search(line).groupdict()["value"]
 
@@ -117,6 +583,66 @@ formatting = re.compile(':<font size=\"17\">( )*(?P<content>.*)</font>')
 for key in statuses:
     if formatting.search(statuses[key]):
         statuses[key] = formatting.search(statuses[key]).groupdict()["content"]
+
+# add investment statuses automatically
+investmentRegex = re.compile('AMER_DELDUM_(?P<attr>.*)PERC_(?P<value>.*)')
+attrs = {
+    "STR": "Strength",
+    "FIN": "Finesse",
+    "INT": "Intelligence",
+    "CON": "Constitution",
+    "MEM": "Memory",
+    "WIT": "Wits"
+}
+for key in statuses:
+    search = investmentRegex.search(key)
+    if search:
+        DELDUMS[key] = [
+            {
+                "type": "extendedStat",
+                "id": "PercAttributeIncrease_" + attrs[search.groupdict()["attr"]],
+                "value": int(search.groupdict()["value"])
+            }
+        ]
+
+# convert rune boosts to the format we use in the app
+for key in runes:
+    rune = runes[key]
+    rune["boosts"] = {"weapon": [], "armor": []}
+    if "real_weaponEffect" in rune:
+        if rune["real_weaponEffect"]["prop"] in DELDUMS:
+            rune["boosts"]["weapon"] += DELDUMS[rune["real_weaponEffect"]["prop"]]
+        rune["weaponBoostString"] = statuses[rune["real_weaponEffect"]["prop"]]
+
+    if "real_armorEffect" in rune:
+        if rune["real_armorEffect"]["prop"] in DELDUMS:
+            rune["boosts"]["armor"] += DELDUMS[rune["real_armorEffect"]["prop"]]
+        rune["armorBoostString"] = statuses[rune["real_armorEffect"]["prop"]]
+
+    if "weaponEffect_boosts" in rune:
+        for statId in rune["weaponEffect_boosts"]:
+            if statId in REAL_STAT_IDS:
+                rune["boosts"]["weapon"] += [{
+                    "type": "flexStat",
+                    "id": REAL_STAT_IDS[statId],
+                    "value": int(rune["weaponEffect_boosts"][statId])
+                }]
+    if "armorEffect_boosts" in rune:
+        for statId in rune["armorEffect_boosts"]:
+            if statId in REAL_STAT_IDS:
+                rune["boosts"]["armor"] += [{
+                    "type": "flexStat",
+                    "id": REAL_STAT_IDS[statId],
+                    "value": int(rune["armorEffect_boosts"][statId])
+                }]
+
+    # remove keys we do not need in the final output
+    scaffoldingKeys = ["weaponEffect_boosts", "armorEffect_boosts", "real_weaponEffect", "real_armorEffect", "armorEffect", "weaponEffect"]
+    if REMOVE_SCAFFOLDING_KEYS:
+        for scaffoldingKey in scaffoldingKeys:
+            try:
+                rune.pop(scaffoldingKey)
+            except: pass
 
 with open("runes.json", "w") as f:
     f.write(json.dumps(runes, indent=2))
